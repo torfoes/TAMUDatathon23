449050a8d8ad1e42212c5fdb7842d1e7728b8fd8
MikamiV <buninsenshi@gmail.com>
2023-10-29 04:13:46 -0500

    Howdy!

--- PictionaryPlungerStarterCode/solution.py
+++ PictionaryPlungerStarterCode/solution.py
@@ -1,38 +1,10 @@
 # IMPORTANT
 # unless you're willing to change the run.py script, keep the new_case, guess, and add_score methods.
 
-# Solution.py
-import torch
 import numpy as np
-from torchvision.transforms import transforms
-import torch.nn as nn
+import tensorflow as tf
 import cv2
-
-
-# CNN model definition (assuming a simple CNN architecture)
-class CNN(nn.Module):
-    def __init__(self, num_classes=345):
-        super(CNN, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
-        self.drop_out = nn.Dropout()
-        self.fc1 = nn.Linear(7 * 7 * 64, 1000)
-        self.fc2 = nn.Linear(1000, num_classes)
-
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.drop_out(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        return out
+from tensorflow.keras import layers, Model
 
 
 # Helper function to convert strokes to image format
@@ -44,22 +16,62 @@ def strokes_to_image(strokes):
     return image
 
 
+# Encoder
+class Encoder(Model):
+    def __init__(self, enc_rnn_size=256, z_size=128):
+        super(Encoder, self).__init__()
+        self.rnn = layers.LSTM(enc_rnn_size, return_sequences=False)
+        self.fc_mu = layers.Dense(z_size)
+        self.fc_logvar = layers.Dense(z_size)
+
+    def call(self, x):
+        h = self.rnn(x)
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)
+        return mu, logvar
+
+
+# Decoder
+class Decoder(Model):
+    def __init__(self, dec_rnn_size=512, num_mixture=20, z_size=128):
+        super(Decoder, self).__init__()
+        self.rnn = layers.LSTM(dec_rnn_size, return_sequences=True)
+        self.fc = layers.Dense(num_mixture)
+        self.fc_z = layers.Dense(dec_rnn_size)
+
+    def call(self, x, z):
+        z_input = self.fc_z(z)
+        x = tf.concat([x, tf.tile(tf.expand_dims(z_input, 1), [1, tf.shape(x)[1], 1])], axis=-1)
+        out = self.rnn(x)
+        out = self.fc(out)
+        return out
+
+
+# SketchRNN Model
+class SketchRNN(Model):
+    def __init__(self, num_classes=345, enc_rnn_size=256, dec_rnn_size=512, z_size=128, num_mixture=20):
+        super(SketchRNN, self).__init__()
+        self.encoder = Encoder(enc_rnn_size, z_size)
+        self.decoder = Decoder(dec_rnn_size, num_mixture, z_size)
+        self.fc = layers.Dense(num_classes)
+
+    def call(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        out = self.decoder(x, z)
+        out = self.fc(out)
+        return out
+
+    def reparameterize(self, mu, logvar):
+        eps = tf.random.normal(shape=mu.shape)
+        return eps * tf.exp(logvar * .5) + mu
+
+
 class Solution:
     def __init__(self):
-        self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(10),
-            transforms.RandomAffine(degrees=10, translate=(0.1, 0.1)),
-            transforms.ToTensor()
-        ])
-
         self.num_classes = 345  # Assuming there are 345 classes, modify as needed
-        self.model = CNN(num_classes=self.num_classes)
-
+        self.model = SketchRNN(num_classes=self.num_classes)
         # Assuming the model weights are loaded somewhere, if needed
-
-        self.model.eval()
 
     # This is a signal that a new drawing is about to be sent
     def new_case(self):
@@ -69,12 +81,14 @@ class Solution:
     def guess(self, x: list[int], y: list[int]) -> str:
         strokes = [x, y]
         image = strokes_to_image(strokes)
-        image_tensor = self.transform(image).unsqueeze(0)
+        # Convert the image to a tensor
+        image_tensor = tf.convert_to_tensor(image, dtype=tf.float32)
+        image_tensor = tf.expand_dims(image_tensor, axis=0)
         outputs = self.model(image_tensor)
-        _, predicted = torch.max(outputs.data, 1)
+        predicted = tf.argmax(outputs, axis=1).numpy()
         # Return the label name based on the prediction
         # Here, you'd map the predicted index to its corresponding label name
-        return self.label_to_name[predicted.item()]
+        return self.label_to_name[predicted[0]]
 
     # This function is called when you get
     def add_score(self, score: int):
